cbd96187fddff6005c6280997ac7a8bf0401a972
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2026-05-06 17:20:29 +0200

    Feat: MILESTONE 2 DONE ! <3

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,39 @@ import Card from './components/Card'
 import { data } from 'react-router-dom'
 
 function App() {
-
-
   useEffect(() => {
-
     getUsers()
-
-
   }, [])
+
   const [users, setUsers] = useState([])
-
-
+  const [search, setSearch] = useState("")
+  const [filterArray, setFilterArray] = useState([])
   async function getUsers() {
     const res = await fetch("http://localhost:3333/politicians");
     const data = await res.json();
     setUsers(data)
-
-
+    setFilterArray(data)
   }
 
+  function filters() {
+
+    if (search !== "") {
+      console.log("valore input iniziale:", search);
 
 
- 
+      const filterUser = users.filter(u => u.name.toLowerCase().includes(search.toLowerCase()) || u.biography.toLowerCase().includes(search.toLowerCase()))
+      setFilterArray(filterUser)
+      setSearch("")
+      console.log("valore input dopo:", search);
+
+    }
+  }
+
+  function resetFilters() {
+    if (search === "") {
+      setFilterArray(users)
+    }
+  }
 
 
 
@@ -35,9 +46,26 @@ function App() {
 
       </div>
       <div className="container">
+        <div className='search-bar'>
+          <form onSubmit={(event) => { event.preventDefault() }}>
+            <input
+              value={search}
+              onChange={(e) => { setSearch(e.target.value) }}
+              type="text"
+              placeholder='Cerca per nome / biografia' />
+            <button
+              onClick={() => { filters() }}
+              type='submit'>INVIA</button>
+            <button
+              onClick={() => { resetFilters() }}
+            >Cancella filtri</button>
+          </form>
+
+        </div>
+
         {users.length > 0 && (
           <>
-            {users.map((u, i) => (
+            {filterArray.map((u, i) => (
               <Card user={u} key={i} />
             ))}
           </>
